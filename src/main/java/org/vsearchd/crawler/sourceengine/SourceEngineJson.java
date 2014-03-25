b39d57d9ed4c4bf742c748d9a67a1ceb06c5f532
Michael Kassnel <mickassnel@vsearchd.org>
2014-03-25 20:12:08 +0100

    added SourceEngineJSON (json2xml converter)

---
+++ src/main/java/org/vsearchd/crawler/sourceengine/SourceEngineJson.java
@@ -0,0 +1,45 @@
+package org.vsearchd.crawler.sourceengine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.json.JSONObject;
+import org.json.XML;
+
+public class SourceEngineJson extends SourceEngineBase {
+	
+	private String line = null;
+	private String jsonStr = null;
+	private StringBuilder stringBuilder = new StringBuilder();
+	private BufferedReader bufferedReader = null;
+
+	public SourceEngineJson() {
+
+	}
+
+	@Override
+	public String getSource(InputStream inputStream) throws IOException {
+		line = null;
+
+		bufferedReader = new BufferedReader(new InputStreamReader(inputStream,
+				this.getSourceEncoding()));
+
+		while ((line = bufferedReader.readLine()) != null) {
+			stringBuilder.append(line);
+		}
+		bufferedReader.close();
+		jsonStr = stringBuilder.toString();
+		stringBuilder.setLength(0);
+		
+		JSONObject json = new JSONObject(jsonStr);
+		return XML.toString(json) ;	
+	}
+
+	@Override
+	public void Reset() {
+		super.Reset();
+	}
+
+}
